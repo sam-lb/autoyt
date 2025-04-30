4370d3f715738f0e1e7fce050f5cc2859fc3a554
sam-lb <samlbrunacini@gmail.com>
2025-04-30 15:27:53 -0400

    wrote the main prompt and implemented title formatting into the request

--- main.py
+++ main.py
@@ -2,15 +2,43 @@ import os
 from dotenv import load_dotenv
 from google import genai
 
-load_dotenv(dotenv_path="key.env")
-key = os.getenv("API_KEY")
-print("key loaded.")
 
-client = genai.Client(api_key=key)
+def make_request(prompt):
+    return client.models.generate_content(
+        model="gemini-2.0-flash",
+        contents=prompt
+    )
 
-response = client.models.generate_content(
-    model="gemini-2.0-flash",
-    contents="Hello Gemini, I am testing if my API requests are working. If you are reading this, you are getting a request from my code via the API. Is it working?"
-)
+if __name__ == "__main__":
+    load_dotenv(dotenv_path="key.env")
+    key = os.getenv("API_KEY")
+    print("key loaded.")
 
-print(response.text)
+    MAKE_REQUEST = False
+    client = genai.Client(api_key=key)
+
+    with open("main_prompt.txt", "r") as f:
+        prompt_template = f.read().rstrip()
+
+    titles = [
+        "The chicken crossed the road",
+        "Cow tipping epidemic in northern wisconsin",
+        "Beedle the bard"
+    ]
+
+    initial_prompt = prompt_template.format(
+        "\n".join(["Title {}: {}".format(i, title) for i, title in enumerate(titles, start=1)])
+    )
+    
+    print("prompt:\n", initial_prompt)
+    if MAKE_REQUEST:
+        print("making request...")
+        response = make_request(initial_prompt).text.strip()
+        print("response:")
+        print(response)
+        try:
+            print("The model chose the following title: {}".format(titles[int(response) - 1]))
+        except ValueError:
+            print("The model failed to choose a number")
+        except IndexError:
+            print("The model chose a title index that was too high")
